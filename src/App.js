3220ec6967602c2ebe8987a841064c1b707e7853
Tapu106 <tapusust993@gmail.com>
2020-08-31 16:56:02 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { LoggedOut, LoginButton, LoggedIn } from "@solid/react";
+import DashBoard from "./components/DashBoard";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <LoggedOut>
+          <section className="section">
+            <p className="content">
+              Please connect to your Pod to start taking notes.
+            </p>
+            <p className="content">
+              <LoginButton
+                popup="popup.html"
+                className="button is-large is-primary"
+              >
+                Connect
+              </LoginButton>
+            </p>
+          </section>
+        </LoggedOut>
+        <LoggedIn>
+          <DashBoard />
+        </LoggedIn>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/DashBoard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { LogoutButton } from "@solid/react";
+
+const dashBoard = (props) => (
+  <>
+  <div>MENU</div>
+  <footer className="footer has-text-right">
+    <LogoutButton className="button" />
+  </footer>
+  </>
+);
+
+export default dashBoard;
